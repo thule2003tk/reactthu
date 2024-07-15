c813e18cc2accdae3b3453e06677385f653008d8
thule2003tk <164979042+thule2003tk@users.noreply.github.com>
2024-07-16 00:42:56 +0700

    thu

--- TestExam/ltht-2210900126-de01-app/src/LthtApp.js
+++ TestExam/ltht-2210900126-de01-app/src/LthtApp.js
@@ -1,23 +1,58 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import LthtListTableName from './ltht_components/LthtListTableName'
 import axios from './ltht_apis/ltht-2210900126'
+import LthtFormTableName from './ltht_components/LthtFormTableName';
 
 export default function LthtApp() {
-  //đọc dữ  liệu tư api
-  const [lthtListTableName, setLthtListTableName] = React.useState([])
+//Doc du lieu tu API
+  const [lthtListTableName, setLthtListTableName] = useState([]);
   const lthtGetTableName = async () => {
-    let lthtResp =axios.get("lthtTableName");
-    console.log(lthtResp.data);
-    setLthtListTableName(lthtResp.data);
+    let lthtRes = await axios.get("lthtTableName");
+    setLthtListTableName(lthtRes.data);
+    console.log("App data:", lthtRes.data);
+    setLthtListTableName(lthtRes.data);
   }
+
   useEffect(() => {
     lthtGetTableName();
-  }, [])
+  }, []);
+
+
+  //Ham Xoa
+  const lthtHandleDelete = async (lthtId) => {
+    let lthtRes = await axios.delete("lthtTableName/"+lthtId);
+    lthtGetTableName();
+  }
+
+  const lthtObjTableName = {
+    "lthtId": "",
+    "lthtTbName": "",
+    "lthtTbEmail": "",
+    "lthtTbPhone": "",
+    "lthtTbStatus": true
+  }
+
+  const [lthtTableName, setLthtTableName] = useState(lthtObjTableName);
+
+  const lthtHandleAdd = ()=>
+  {
+    lthtGetTableName();
+  }
+
+
+  const lthtHandleEdit = (lthtObjEditTableName) => {
+    setLthtTableName(lthtObjEditTableName);
+    lthtGetTableName();
+  }
+
   return (
-    <div className='container border my -3'>
-      <h1>bài đánh giá hết học phần - Lê thương hoài thu: 2210900126</h1>
+    <div className='container border my-3'> 
+      <h1>Bai danh gia het hoc phan - Le Thuong Hoai Thu: 2210900126</h1>
       <hr/>
-      <LthtListTableName renderLthtListTableName={lthtListTableName}/>
+      <LthtListTableName renderLthtListTableName = {lthtListTableName} onLthtDelete={lthtHandleDelete} onLthtEdit={lthtHandleEdit} onLthtAdd={lthtHandleAdd}/>
+      <hr/>
+      <LthtFormTableName renderLthtTableName = {lthtTableName} onLthtEdit={lthtHandleEdit} onLthtAdd={lthtHandleAdd}/> 
+      <hr/>
     </div>
   )
 }
